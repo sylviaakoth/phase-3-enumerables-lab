5b835da422d52d531722ff21d4ae005c40161b5f
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-10-21 14:14:45 +0300

    done

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -1,66 +1,69 @@
-require 'pry'
+require_relative 'spec_helper'
+require_relative '../enumerables'
 
-# this method returns an array of hashes, which we'll use in the other methods
-def spicy_foods 
-  [
-    { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 },
-    { name: 'Buffalo Wings', cuisine: 'American', heat_level: 3 },
-    { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 }
-  ]
-end
+describe '#get_names' do
 
-# given an array of spicy foods, **return an array of strings**
-# with the names of each spicy food
-def get_names(spicy_foods)
-  spicy_foods.map { |food| food[:name]}
-end
-
-# given an array of spicy foods, **return an array of hashes** 
-# where the heat level of the food is greater than 5
-def spiciest_foods(spicy_foods)
-  spicy_foods.filter {|food| food[:heat_level] > 5}
-end
-
-# given an array of spicy foods, **output to the terminal**
-# each spicy food in the following format: 
-# Buffalo Wings (American) | Heat Level: 🌶🌶🌶
-# HINT: you can use * with a string to produce the correct number of 🌶 emoji. 
-# "hello" * 3 == "hellohellohello"
-def print_spicy_foods(spicy_foods)
-  spicy_foods.map do |food|
-    emojis = "🌶" * food[:heat_level]
-    puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{emojis}"
-  end
-end
-
-# given an array of spicy foods and a string representing a cuisine, **return a single hash**  
-# for the spicy food whose cuisine matches the cuisine being passed to the method
-def get_spicy_food_by_cuisine(spicy_foods, cuisine)
-  spicy_foods.find { |food| food[:cuisine] == cuisine}
-end
-
-# Given an array of spicy foods, **return an array of hashes** 
-# sorted by heat level from lowest to highest
-def sort_by_heat(spicy_foods)
-  spicy_foods.sort { |heat1, heat2| heat1[:heat_level] <=> heat2[:heat_level] }
-end
-
-# given an array of spicy foods, output to the terminal ONLY 
-# the spicy foods that have a heat level greater than 5, in the following format: 
-# Buffalo Wings (American) | Heat Level: 🌶🌶🌶
-# HINT: Try to use methods you've already written to solve this!
-def print_spiciest_foods(spicy_foods)
-  spicy_foods.filter do |food|
-    if food[:heat_level] > 5
-      emojis = "🌶" * food[:heat_level]
-      puts "#{food[:name]} (#{food[:cuisine]}) | Heat Level: #{emojis}"
-    end
+  it 'returns an array of names' do
+    expect(get_names(spicy_foods)).to eq(["Green Curry", "Buffalo Wings", "Mapo Tofu"])
   end
 
 end
 
-# given an array of spicy foods, return an integer representing 
-# the average heat level of all the spicy foods in the array
-def average_heat_level(spicy_foods)
-  (spicy_foods.map { |food| food[:heat_level] }).sum / spicy_foods.length
+describe '#spiciest_foods' do
+  
+  it 'return an array of hashes where the heat level of the food is greater than 5' do
+    expect(spiciest_foods(spicy_foods)).to eq([
+      { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 },
+      { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 } 
+    ])
+  end
+
+end
+
+describe '#print_spicy_foods' do
+  
+  it 'outputs the spicy foods to the terminal in the specified format' do
+    output_str = "Green Curry (Thai) | Heat Level: 🌶🌶🌶🌶🌶🌶🌶🌶🌶\nBuffalo Wings (American) | Heat Level: 🌶🌶🌶\nMapo Tofu (Sichuan) | Heat Level: 🌶🌶🌶🌶🌶🌶\n"
+    expect { print_spicy_foods(spicy_foods) }.to output(output_str).to_stdout
+  end
+  
+end
+
+describe '#get_spicy_food_by_cuisine' do
+  
+  it 'returns a single hash for the spicy food whose cuisine matches the cuisine being passed to the method' do
+    expect(get_spicy_food_by_cuisine(spicy_foods, "Sichuan")).to eq({ name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 })
+    expect(get_spicy_food_by_cuisine(spicy_foods, "Thai")).to eq({ name: 'Green Curry', cuisine: 'Thai', heat_level: 9 })
+  end
+
+end
+
+describe '#sort_by_heat' do
+  
+  it 'return an array of hashes sorted by heat level from lowest to highest' do
+    sorted_array = [
+      { name: 'Buffalo Wings', cuisine: 'American', heat_level: 3 },
+      { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 },
+      { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 }
+    ]
+    expect(sort_by_heat(spicy_foods)).to eq(sorted_array)
+  end
+
+end
+
+describe '#print_spiciest_foods' do
+  
+  it 'outputs ONLY the spicy foods that have a heat level greater than 5 to the terminal in the specified format' do
+    output_str = "Green Curry (Thai) | Heat Level: 🌶🌶🌶🌶🌶🌶🌶🌶🌶\nMapo Tofu (Sichuan) | Heat Level: 🌶🌶🌶🌶🌶🌶\n"
+    expect { print_spiciest_foods(spicy_foods) }.to output(output_str).to_stdout
+  end
+  
+end
+
+describe '#average_heat_level' do
+  
+  it 'returns an integer representing the average heat level of all the spicy foods in the array' do
+    expect(average_heat_level(spicy_foods)).to eq(6)
+  end
+  
 end
